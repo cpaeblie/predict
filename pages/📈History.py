dad191a295c2fe18c858a25b8bca87e320e53d23
cpaeblie <168696426+cpaeblie@users.noreply.github.com>
2024-11-02 15:37:37 +0700

    Update 📈History.py

--- pages/📈History.py
+++ pages/📈History.py
@@ -117,7 +117,7 @@ from sklearn.model_selection import RandomizedSearchCV
 # Create the title and description
 st.set_page_config(page_title="📈History", page_icon="📈")
 # History Page
-st.title("📈History")
+st.title("History")
 st.write("This section displays line charts of each column in the dataset, providing insights into trends over time.")
 
 # Date vs CPA
