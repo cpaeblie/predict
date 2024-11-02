5e8c3933e236a59d4068fcfd8dffb70221b3d9e5
cpaeblie <168696426+cpaeblie@users.noreply.github.com>
2024-11-02 14:06:51 +0700

    Update dataset.py

--- pages/dataset.py
+++ pages/dataset.py
@@ -125,116 +125,47 @@ st.set_page_config(page_title="CPA Prediction App", page_icon="🔎")
 st.title("CPA Prediction App 🔎")
 st.sidebar.title("Menu")
 menu = st.sidebar.selectbox("Select a page:", ["History", "Dataset", "Prediction"])
+# Dataset Page
+st.title("Dataset")
+st.write("Here is the dataset used for the CPA prediction.")
+st.dataframe(df_ori)
 
-    # Dataset Page
-    st.title("Dataset")
-    st.write("Here is the dataset used for the CPA prediction.")
-    st.dataframe(df_ori)
-	# Correlation Analysis
-    st.write("This section displays scatter plots illustrating the correlations between key features in the dataset.")
+# Correlation Analysis
+st.write("This section displays scatter plots illustrating the correlations between key features in the dataset.")
 
-    # Define the specific pairs to analyze
-    pairs = [
-        ('Cost', 'CPA'),
-        ('CPC (Destination)', 'CPA'),
-        ('CPM', 'CPA'),
-        ('CTR (Destination)', 'CPA')
-    ]
-    
-    for feature1, feature2 in pairs:
-        # Scatter plot
-        plt.figure(figsize=(10, 6))
-        sns.scatterplot(data=df_ori, x=feature1, y=feature2)
-        plt.title(f'Scatter Plot: {feature1} vs {feature2}')
-        plt.xlabel(feature1)
-        plt.ylabel(feature2)
-        plt.axhline(y=0, color='gray', linestyle='--', linewidth=0.5)
-        plt.axvline(x=0, color='gray', linestyle='--', linewidth=0.5)
-        plt.grid()
+# Define the specific pairs to analyze
+pairs = [
+    ('Cost', 'CPA'),
+    ('CPC (Destination)', 'CPA'),
+    ('CPM', 'CPA'),
+    ('CTR (Destination)', 'CPA')
+]
 
-        # Show the plot in Streamlit
-        st.pyplot(plt)
+for feature1, feature2 in pairs:
+    # Scatter plot
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(data=df_ori, x=feature1, y=feature2)
+    plt.title(f'Scatter Plot: {feature1} vs {feature2}')
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.axhline(y=0, color='gray', linestyle='--', linewidth=0.5)
+    plt.axvline(x=0, color='gray', linestyle='--', linewidth=0.5)
+    plt.grid()
 
-        # Calculate correlation
-        correlation_value = df_ori[feature1].corr(df_ori[feature2])
-        st.write(f"The correlation coefficient between **{feature1}** and **{feature2}** is **{correlation_value:.2f}**. This indicates a {'positive' if correlation_value > 0 else 'negative'} correlation.")
+    # Show the plot in Streamlit
+    st.pyplot(plt)
 
-        # Additional descriptions for specific pairs
-        if feature1 == 'Cost' and feature2 == 'CPA':
-            st.write("This scatter plot shows the relationship between total Cost and Cost Per Acquisition (CPA). A positive correlation suggests that as total spending increases, the cost to acquire each customer may also increase.")
-        elif feature1 == 'CPC (Destination)' and feature2 == 'CPA':
-            st.write("This scatter plot illustrates the relationship between CPC and CPA. A positive correlation may imply that higher costs per click lead to higher costs per acquisition.")
-        elif feature1 == 'CPM' and feature2 == 'CPA':
-            st.write("This scatter plot shows the relationship between CPM and CPA. A positive correlation might suggest that as the cost per 1,000 impressions increases, the cost per acquisition also tends to increase.")
-        elif feature1 == 'CTR (Destination)' and feature2 == 'CPA':
-            st.write("This scatter plot illustrates the relationship between CTR and CPA. A negative correlation may suggest that as the click-through rate increases, the cost per acquisition decreases, indicating more effective spending.")
+    # Calculate correlation
+    correlation_value = df_ori[feature1].corr(df_ori[feature2])
+    st.write(f"The correlation coefficient between **{feature1}** and **{feature2}** is **{correlation_value:.2f}**. This indicates a {'positive' if correlation_value > 0 else 'negative'} correlation.")
 
-elif menu == "Prediction":
-
-    st.write("""This is a CPA Prediction App that uses machine learning algorithms to predict the Cost Per Acquisition (CPA) for a given set of input features Cost, CPC (Destination), CPM, CTR (Destination) for the 4 days before tomorrow.
-""")
-    st.write("""
-Enter the Cost, CPC (Destination), CPM, CTR (Destination) at Day 1 until Day 4:
-""")
-# Create the input widgets for the new name
-    new_name_inputs = []
-    with st.form("cpa_form"):
-        for i in range(16):
-            day = (i // 4) + 1
-            metric = i % 4
-            if metric == 0:
-                metric = "Cost"
-            elif metric == 1:
-                metric = "CPC (Destination)"
-            elif metric == 2:
-                metric = "CPM"
-            else:
-                metric = "CTR (Destination)"
-        
-            new_name_input = st.text_input(label=f'{metric} at Day {day}:', key=f'input_{i+16}')
-            new_name_inputs.append(new_name_input)
-        if st.form_submit_button("Predict The CPA!"):
-            # Get the input values
-            new_name = np.array([float(new_name_input) for new_name_input in new_name_inputs]).reshape(-1, X_test.shape[1])
-            # Remaining code...
-
-            # Scale the input features
-            scaler = StandardScaler().fit(X_train_no_nan)
-            X_train_scaled = scaler.transform(X_train_no_nan)
-            X_test_scaled = scaler.transform(new_name)
-
-            # Define the hyperparameter distribution
-            param_dist = {
-                'n_estimators': [10, 50, 100, 200, 500],
-                'max_depth': [None, 10, 20, 30, 40, 50],
-                'min_samples_split': [2, 5, 10, 20, 30],
-                'min_samples_leaf': [1, 2, 4, 8, 16]
-            }
-
-        # Initialize the Random Forest Regressor model
-            model = RandomForestRegressor(random_state=42)
-
-        # Perform hyperparameter tuning using RandomizedSearchCV
-            random_search = RandomizedSearchCV(estimator=model, param_distributions=param_dist, cv=5, scoring='neg_mean_squared_error', verbose=0, n_iter=20, random_state=42)
-            random_search.fit(X_train_scaled, y_train_no_nan)
-
-        # Extract the best model and fit it to the training data
-            best_model = random_search.best_estimator_
-            best_model.fit(X_train_scaled, y_train_no_nan)
-
-        # Make predictions on the test data
-            y_pred = best_model.predict(X_test_scaled)
-            y_pred = np.round(y_pred, 0)
-
-        # Display the predictions in the sidebar
-            st.sidebar.write("Tomorrow's CPA Prediction:")
-            st.sidebar.write(y_pred)
-    st.write("""
-    Don't forget to recheck again before click the button
-""")
-    st.write("""
-    Please refresh the website if you want input new values
-""")
-
-	    
+    # Additional descriptions for specific pairs
+    if feature1 == 'Cost' and feature2 == 'CPA':
+        st.write("This scatter plot shows the relationship between total Cost and Cost Per Acquisition (CPA). A positive correlation suggests that as total spending increases, the cost to acquire each customer may also increase.")
+    elif feature1 == 'CPC (Destination)' and feature2 == 'CPA':
+        st.write("This scatter plot illustrates the relationship between CPC and CPA. A positive correlation may imply that higher costs per click lead to higher costs per acquisition.")
+    elif feature1 == 'CPM' and feature2 == 'CPA':
+        st.write("This scatter plot shows the relationship between CPM and CPA. A positive correlation might suggest that as the cost per 1,000 impressions increases, the cost per acquisition also tends to increase.")
+    elif feature1 == 'CTR (Destination)' and feature2 == 'CPA':
+        st.write("This scatter plot illustrates the relationship between CTR and CPA. A negative correlation may suggest that as the click-through rate increases, the cost per acquisition decreases, indicating more effective spending.")	    
 st.caption('Copyright (c) PT Ebliethos Indonesia 2024')
